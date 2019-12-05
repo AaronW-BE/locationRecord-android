db4660835436b368bb5911bd7c3ae298e002e8d8
aaronwb <zmtzwb@gmail.com>
2019-12-05 16:53:49 +0800

    修改log工具类以支持多参数

--- app/src/main/java/com/robotsme/app/location/utils/MLog.java
+++ app/src/main/java/com/robotsme/app/location/utils/MLog.java
@@ -1,5 +1,6 @@
 package com.robotsme.app.location.utils;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 public class MLog {
@@ -7,15 +8,17 @@ public class MLog {
     public static final boolean IS_DEBUG = true;
     public static final String TAG = "LocationRecord";
 
-    public static void i (String msg) {
+    public static void i (String ...msg) {
         if (IS_DEBUG) {
-            Log.i(TAG, msg);
+            String log = TextUtils.join(",", msg);
+            Log.i(TAG, log);
         }
     }
 
-    public static void e (String msg) {
+    public static void e (String ...msg) {
         if (IS_DEBUG) {
-            Log.e(TAG, msg);
+            String log = TextUtils.join(",", msg);
+            Log.e(TAG, log);
         }
     }
 }
